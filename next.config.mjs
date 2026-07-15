71ee9a170e8415cafae20b1b3b6a556abc3d4011
msyafiulmasruri <msyafiulmasruri@gmail.com>
2026-07-15 13:36:16 +0700

    fix: replace new root dependencies and package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  allowedDevOrigins: ['192.168.1.22'],
+  allowedDevOrigins: ['most-citadel-distill.ngrok-free.dev'],
 };
 
 export default nextConfig;
